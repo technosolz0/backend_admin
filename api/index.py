f334979932f3651848790697b7efe5ceb7ec7a00
Samir <mksamir013@gmail.com>
2025-07-20 14:10:49 +0530

    Fix path for vercel deployment

--- api/index.py
+++ api/index.py
@@ -1,13 +1,5 @@
-import sys
-import os
 
-# Add path to app folder
-sys.path.append(os.path.join(os.path.dirname(__file__), "../app"))
-
-# Import FastAPI app
 from app.main import app
-
-# Mangum to wrap FastAPI for AWS Lambda / Vercel
 from mangum import Mangum
 
 handler = Mangum(app)
