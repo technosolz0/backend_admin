6b204c3a72e416e5f3030977bf731d11c99b36bd
Samir <mksamir013@gmail.com>
2025-07-20 13:15:07 +0530

    update

--- api/index.py
+++ api/index.py
@@ -1,12 +1,13 @@
-# backend_admin/api/index.py
-
 import sys
 import os
 
-# Add backend_admin/app to the path
+# Add path to app folder
 sys.path.append(os.path.join(os.path.dirname(__file__), "../app"))
 
-from main import app
+# Import FastAPI app
+from app.main import app
+
+# Mangum to wrap FastAPI for AWS Lambda / Vercel
 from mangum import Mangum
 
 handler = Mangum(app)
