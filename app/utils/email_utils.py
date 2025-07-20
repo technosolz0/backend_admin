7d6267e793f2bdd46070522a8488a1ef9c311812
technosolz0 <technosolz01@gmail.com>
2025-07-20 15:53:15 +0530

    fix: remove mangum from main.py for Vercel5

--- app/utils/email_utils.py
+++ app/utils/email_utils.py
@@ -28,17 +28,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def send_email_otp(receiver_email: str, otp: str):
-    sender_email = os.getenv("EMAIL_USERNAME")
-    sender_password = os.getenv("EMAIL_PASSWORD")
+    # sender_email = os.getenv("EMAIL_USERNAME")
+    # sender_password = os.getenv("EMAIL_PASSWORD")
 
     message = MIMEMultipart("alternative")
     message["Subject"] = "Your OTP Code"
-    message["From"] = sender_email
-    message["To"] = receiver_email
+    # message["From"] = sender_email
+    # message["To"] = receiver_email
 
     text = f"Your OTP code is: {otp}"
     message.attach(MIMEText(text, "plain"))
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(sender_email, sender_password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
+        pass
+        # server.login(sender_email, sender_password)
+        # server.sendmail(sender_email, receiver_email, message.as_string())
