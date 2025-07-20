87f66a107f8ba16789e19f544f00b5f68e3bfe1b
technosolz0 <technosolz01@gmail.com>
2025-07-20 14:37:30 +0530

    fix: remove mangum from main.py for Vercel3

--- api/index.py
+++ api/index.py
@@ -1,4 +1,14 @@
+import sys
+import os
+
+# Add path to app folder
+sys.path.append(os.path.join(os.path.dirname(__file__), "../app"))
+
+# Import FastAPI app
 from app.main import app
+
+# Mangum to wrap FastAPI for AWS Lambda / Vercel
 from mangum import Mangum
 
+# 👇 This is critical for Vercel
 handler = Mangum(app)
